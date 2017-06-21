fe9da2e0e5a05d58966f9d22cd5530a079e5a61b
mbialeckiz <marcinbialeckiz@gmail.com>
2017-06-21 22:03:26 +0200

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,11 @@
 var a, h;
 
 function getTriangleArea(a, h) {
-	if (a,h > 0) {
-		// wykonanie funkcji
-		return (a*h/2);
+	if (a > 0 && h > 0) {
+  		return a*h/2
 	} else {
-		console.log('Nieprawidłowe dane');
-		alert('Nieprawidłowe dane');
+  	console.log('Nieprawidłowe dane')
+    alert('Nieprawidłowe dane')
 	}
 }
 
@@ -16,4 +15,8 @@ console.log( getTriangleArea(10,6) )
 
 var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(100, 1);
-var triangle2Area = getTriangleArea(3, 4);
+var triangle3Area = getTriangleArea(3, 4);
+
+console.log(triangle1Area)
+console.log(triangle2Area)
+console.log(triangle3Area)
